d3b4976ce3c0b9068a2195c2414fef3fe7e02755
zine yu <zine.xlws@gmail.com>
2025-02-09 21:08:13 +0800

    25/2/9

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -22,8 +22,8 @@ return {
 		keymap = {
 			preset = "default",
 			["<Tab>"] = { "accept", "fallback" },
-			["<C-j>"] = { "select_next", "fallback" },
-			["<C-k>"] = { "select_prev", "fallback" },
+			["<C-]>"] = { "select_next", "fallback" },
+			["<C-[>"] = { "select_prev", "fallback" },
 		},
 		appearance = {
 			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -34,20 +34,20 @@ return {
 			-- Adjusts spacing to ensure icons are aligned
 			nerd_font_variant = "mono",
 		},
-		snippets = {
-			expand = function(snippet)
-				require("luasnip").lsp_expand(snippet)
-			end,
-			active = function(filter)
-				if filter and filter.direction then
-					return require("luasnip").jumpable(filter.direction)
-				end
-				return require("luasnip").in_snippet()
-			end,
-			jump = function(direction)
-				require("luasnip").jump(direction)
-			end,
-		},
+		-- snippets = {
+		-- 	expand = function(snippet)
+		-- 		require("luasnip").lsp_expand(snippet)
+		-- 	end,
+		-- 	active = function(filter)
+		-- 		if filter and filter.direction then
+		-- 			return require("luasnip").jumpable(filter.direction)
+		-- 		end
+		-- 		return require("luasnip").in_snippet()
+		-- 	end,
+		-- 	jump = function(direction)
+		-- 		require("luasnip").jump(direction)
+		-- 	end,
+		-- },
 		sources = {
 			default = { "snippets", "vsnip", "lsp", "path", "buffer" },
 			-- optionally disable cmdline completions
