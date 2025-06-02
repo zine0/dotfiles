7fbe209f1f64e53b5f38ed3b9f965d0d93dbc870
zine yu <zine.xlws@gmail.com>
2025-06-02 20:36:43 +0800

    Setup fish

---
+++ eggs/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias cl clear
+alias hx helix
+alias ls lsd
+
+export GO111MODULE=auto
+
+starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/zine/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
