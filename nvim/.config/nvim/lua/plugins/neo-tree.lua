93c620fd06197091c9689e104903f57d726a0c5c
zine yu <zine.xlws@gmail.com>
2025-12-26 18:09:15 +0800

    change the config of nvim
    Signed-off-by: zine yu <zine.xlws@gmail.com>

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -8,5 +8,19 @@ return {
             "nvim-tree/nvim-web-devicons", -- optional, but recommended
         },
         lazy = false, -- neo-tree will lazily load itself
+        opts = {
+            open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+            use_libuv_file_watcher = true,
+            document_symbols = {
+                window = {
+                    width = 25,
+                },
+            },
+            filesystem = {
+                window = {
+                    width = 25,
+                },
+            },
+        },
     },
 }
